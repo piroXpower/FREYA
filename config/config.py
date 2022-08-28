022e57c3e104365f0df311c85f51b59cef4a265d
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-08-28 14:19:22 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -70,10 +70,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", None
+    "SUPPORT_CHANNEL", "https://t.me/TheDeadlyBots"
 )  # Example:- https://t.me/TheYukki
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", None
+    "SUPPORT_GROUP", "https://t.me/TheDeadlyBots"
 )  # Example:- https://t.me/YukkiSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
@@ -173,7 +173,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/4f5e749c0b92e61e6a09f.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
